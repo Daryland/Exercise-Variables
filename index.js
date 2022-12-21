676141c420028cad75c8067b75a88b19884753b8
foesublime <rylandcs@gmail.com>
2022-12-21 15:31:04 -0700

    added values to vehicle

---
+++ index.js
@@ -0,0 +1,13 @@
+var vehicle = {
+  make: 'Ford',
+  model: 'car',
+  year: 1990,
+  color: 'black',
+  isManualTransmission: false,
+  numSeats: 4, 
+  mileage: 0,
+}
+
+
+console.log('Value: ', vehicle);
+console.log('Type: ', typeof vehicle);
